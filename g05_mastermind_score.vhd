4a4fabeea6ce13cd4f13f83e5698a1e15e04364b
Felix Dube <felixdube@wpa063026.wireless.mcgill.ca>
2015-10-18 15:15:27 -0400

    made mastermind score encoder

--- g05_mastermind_score.vhd
+++ g05_mastermind_score.vhd
@@ -17,17 +17,12 @@ entity g05_mastermind_score is
 		color_match_score : 	out 	std_logic_vector(2 downto 0);
 		score_code : 			out 	std_logic_vector(3 downto 0)
 	); 
-end g00_mastermind_score;
+end g05_mastermind_score;
 
-architecture behavior of a is
-	component g05_num_matches is
-		 port (
-			  P1, P2, P3, P4 : in std_logic_vector(2 downto 0);
-			  G1, G2, G3, G4 : in std_logic_vector(2 downto 0);
-			  N : out std_logic_vector(2 downto 0)
-		 );
-	end component;
-	
+architecture behavior of g05_mastermind_score is
+
+signal color_matches, exact_matches : std_logic_vector(2 downto 0);
+
 	component g05_color_matches is
 		 port ( 
 			  P1, P2, P3, P4 : in std_logic_vector(2 downto 0);
@@ -44,5 +39,17 @@ architecture behavior of a is
 			num_color_matches : in std_logic_vector(2 downto 0)
 		);
 	end component;
+	
+begin
+	num_matches : g05_color_matches
+		port map(P1 => P1, P2 => P2, P3 => P3, P4 => P4, G1 => G1, G2 => G2, G3 => G3, G4 => G4, 
+					num_exact_matches => exact_matches, num_color_matches => color_matches);
+		
+	score : g05_score_encoder
+		port map(num_exact_matches => exact_matches, num_color_matches => color_matches, score_code => score_code);
+		
+	color_match_score <= color_matches;
+	exact_match_score <= exact_matches;
+	
 
 end behavior;
